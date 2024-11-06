bf7fe5cb105ecae3efd79fcc93dfaf598e77e4fe
jbucsa <justin.bucsa@gmail.com>
2024-11-06 16:28:10 -0600

    Uploaded Spring Boot Initalized project, ie the project template.

---
+++ run_tracker/src/main/java/dev/bucsa/run_tracker/RunTrackerApplication.java
@@ -0,0 +1,13 @@
+package dev.bucsa.run_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunTrackerApplication.class, args);
+	}
+
+}
