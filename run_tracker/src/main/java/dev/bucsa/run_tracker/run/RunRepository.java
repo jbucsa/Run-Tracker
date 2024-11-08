09b2a727437de366f3c93f701780968b561093be
jbucsa <justin.bucsa@gmail.com>
2024-11-08 12:33:55 -0600

    Begun Repository for Run feature.

---
+++ run_tracker/src/main/java/dev/bucsa/run_tracker/run/RunRepository.java
@@ -0,0 +1,24 @@
+package dev.bucsa.run_tracker.run;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.annotation.PostConstruct;
+
+
+public class RunRepository {
+    private List<Run> runs = new ArrayList<>();
+
+    List<Run> findAll() {
+        return runs;
+    }
+
+    @PostConstruct
+    private void init() {
+        runs.add(new Run(1, "First Run", LocalDateTime.now(), LocalDateTime.now().plus(1, ChronoUnit.HOURS), 3, Location.OUTDOOR));
+    
+        runs.add(new Run(2, "Second Run", LocalDateTime.now(), LocalDateTime.now().plus(45, ChronoUnit.MINUTES), 2, Location.INDOOR)); 
+    }
+}
