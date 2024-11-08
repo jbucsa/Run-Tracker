b7597a612f1ed1be82a543253f6440da4e6c2237
jbucsa <justin.bucsa@gmail.com>
2024-11-08 10:55:08 -0600

    Based Application set Up.

--- run_tracker/src/main/java/dev/bucsa/run_tracker/Application.java
+++ run_tracker/src/main/java/dev/bucsa/run_tracker/Application.java
@@ -1,9 +1,17 @@
 package dev.bucsa.run_tracker;
 
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import dev.bucsa.run_tracker.run.Location;
+import dev.bucsa.run_tracker.run.Run;
 
 
 
@@ -31,8 +39,19 @@ public class Application {
         // System.out.println(welcomeMessage.getWelcomeMessage());
 
 		SpringApplication.run(Application.class, args);
-		log.info("Application started successfully");
-		log.info("Something Changed");
+		
+		// Note 2.4 
+		// These send out messages to the terminal that are readable and update when the file is saved.
+		// log.info("Application started successfully");
+		// log.info("Something Changed");
+	}
+
+    @Bean
+    CommandLineRunner runner() {
+        return args -> {
+            Run run = new Run(1, "First Run", LocalDateTime.now(), LocalDateTime.now().plus(1, ChronoUnit.HOURS), 3, Location.OUTDOOR);
+            log.info("Run: " + run);
+        };
 	}
 
 }
