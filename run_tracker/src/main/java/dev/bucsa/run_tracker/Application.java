c5ead3b38ea80edb110c0e2d6800a06c71aa8c98
jbucsa <justin.bucsa@gmail.com>
2024-11-08 10:59:37 -0600

    Application files cleaned up. Notes REMOVED

--- run_tracker/src/main/java/dev/bucsa/run_tracker/Application.java
+++ run_tracker/src/main/java/dev/bucsa/run_tracker/Application.java
@@ -14,36 +14,13 @@ import dev.bucsa.run_tracker.run.Location;
 import dev.bucsa.run_tracker.run.Run;
 
 
-
-// Note 2.1
-// import WelcomeMessage;
-
 @SpringBootApplication
 public class Application {
 
-	// Note 2.3 
-	// This the code for a logger
 	private  static final Logger log = LoggerFactory.getLogger(Application.class);
 
 	public static void main(String[] args) {
-		// Note 2.1
-		// This is the simplest way of putting an "Welcome message" from an imported file/package. 
-		// 	var welcomeMessage = new WelcomeMessage();
-		// 	System.out.println(welcomeMessage.getWelcomeMessage());
-
-		// Note 2.2
-		// Below is an example of congifuration control 
-		// ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
-
-		// WelcomeMessage welcomeMessage = context.getBean(WelcomeMessage.class);
-        // System.out.println(welcomeMessage.getWelcomeMessage());
-
 		SpringApplication.run(Application.class, args);
-		
-		// Note 2.4 
-		// These send out messages to the terminal that are readable and update when the file is saved.
-		// log.info("Application started successfully");
-		// log.info("Something Changed");
 	}
 
     @Bean
@@ -53,5 +30,4 @@ public class Application {
             log.info("Run: " + run);
         };
 	}
-
 }
