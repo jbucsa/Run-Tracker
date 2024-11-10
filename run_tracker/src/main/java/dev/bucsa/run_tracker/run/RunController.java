304286a3192c2d170e846f00486d0ff55ea58aca
jbucsa <justin.bucsa@gmail.com>
2024-11-09 18:18:42 -0600

    Update to Run Controller.

--- run_tracker/src/main/java/dev/bucsa/run_tracker/run/RunController.java
+++ run_tracker/src/main/java/dev/bucsa/run_tracker/run/RunController.java
@@ -1,12 +1,24 @@
 package dev.bucsa.run_tracker.run;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RunController {
-    @RequestMapping("/hello")
-    String home(){
-        return "Hello, Runners!!! Lets start";
+    // @RequestMapping("/hello")
+    // String home(){
+    //     return "Hello, Runners!!! Lets start";
+    
+    private final RunRepository runRepository;
+
+    public RunController(RunRepository runRepository){
+        this.runRepository = runRepository;
+    }   
+
+    @GetMapping("/api/runs")
+    List<Run> findAll(){
+        return runRepository.findAll();
     }
 }
